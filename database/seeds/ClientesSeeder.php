6d82df8c2b52276f9992c8b8eeec81c95c95c716
Luis Macias <luismacias.angulo@gmail.com>
2016-07-19 22:40:55 -0500

    Cambios en seeders

--- database/seeds/ClientesSeeder.php
+++ database/seeds/ClientesSeeder.php
@@ -14,29 +14,51 @@ class ClientesSeeder extends Seeder
 	{
 		$faker = Faker\Factory::create();
 		
-		for ($i = 0; $i < 1200; $i++)
-		{
-			DB::table('cl_clientes')->insert([
-				'razonsocial'  => mb_strtoupper($faker->company) . ' - ' . $faker->randomNumber(4),
-				'rfc'          => mb_strtoupper(substr($faker->md5, 0, 13)),
-				'prospecto'    => $faker->randomElement([0, 1]),
-				'distribuidor' => $faker->randomElement([0, 1]),
-				'email'        => $faker->safeEmail,
-				'telefono'     => $faker->randomNumber(9) . $faker->randomNumber(1),
-				'telefono2'    => '',
-				'calle'        => $faker->streetName,
-				'noexterior'   => $faker->buildingNumber,
-				'nointerior'   => '',
-				'colonia'      => $faker->cityPrefix,
-				'cp'           => $faker->postcode,
-				'ciudad'       => $faker->city,
-				'municipio'    => '',
-				'estado'       => $faker->state,
-				'pais'         => 'México',
-				'online'       => 1,
-				'created_at'   => date('Y-m-d H:i:s'),
-				'updated_at'   => date('Y-m-d H:i:s')
-			]);
-		}
+		DB::table('cl_clientes')->insert([
+			'razonsocial'  => 'EMPRESA DE MACIAS',
+			'rfc'          => 'AAA000AAAA123',
+			'prospecto'    => 0,
+			'distribuidor' => 1,
+			'email'        => 'luismacias.angulo@gmail.com',
+			'telefono'     => '3929418119',
+			'telefono2'    => '',
+			'calle'        => 'Sócrates',
+			'noexterior'   => '244',
+			'nointerior'   => '',
+			'colonia'      => 'Paso Blanco',
+			'cp'           => '47810',
+			'ciudad'       => 'Ocotlán',
+			'municipio'    => '',
+			'estado'       => 'Jalisco',
+			'pais'         => 'México',
+			'online'       => 1,
+			'created_at'   => date('Y-m-d H:i:s'),
+			'updated_at'   => date('Y-m-d H:i:s')
+		]);
+		
+//		for ($i = 0; $i < 1200; $i++)
+//		{
+//			DB::table('cl_clientes')->insert([
+//				'razonsocial'  => mb_strtoupper($faker->company) . ' - ' . $faker->randomNumber(4),
+//				'rfc'          => mb_strtoupper(substr($faker->md5, 0, 13)),
+//				'prospecto'    => $faker->randomElement([0, 1]),
+//				'distribuidor' => $faker->randomElement([0, 1]),
+//				'email'        => $faker->safeEmail,
+//				'telefono'     => $faker->randomNumber(9) . $faker->randomNumber(1),
+//				'telefono2'    => '',
+//				'calle'        => $faker->streetName,
+//				'noexterior'   => $faker->buildingNumber,
+//				'nointerior'   => '',
+//				'colonia'      => $faker->cityPrefix,
+//				'cp'           => $faker->postcode,
+//				'ciudad'       => $faker->city,
+//				'municipio'    => '',
+//				'estado'       => $faker->state,
+//				'pais'         => 'México',
+//				'online'       => 1,
+//				'created_at'   => date('Y-m-d H:i:s'),
+//				'updated_at'   => date('Y-m-d H:i:s')
+//			]);
+//		}
 	}
 }
--- database/seeds/EjecutivosSeeder.php
+++ database/seeds/EjecutivosSeeder.php
@@ -25,19 +25,18 @@ class EjecutivosSeeder extends Seeder
 			'updated_at'   => date('Y-m-d H:i:s'),
 		]);
 		
-		for ($i=0; $i<2; $i++)
-		{
-			DB::table('ec_ejecutivos')->insert([
-				'nombre'       => $faker->name,
-				'apellido'     => $faker->lastName,
-				'email'        => $faker->email,
-				'password'     => bcrypt('secret'),
-				'oficina_id'      => 1,
-				'departamento_id' => 1,
-				'created_at'   => date('Y-m-d H:i:s'),
-				'updated_at'   => date('Y-m-d H:i:s'),
-			]);
-		}
-
+//		for ($i=0; $i<2; $i++)
+//		{
+//			DB::table('ec_ejecutivos')->insert([
+//				'nombre'       => $faker->name,
+//				'apellido'     => $faker->lastName,
+//				'email'        => $faker->email,
+//				'password'     => bcrypt('secret'),
+//				'oficina_id'      => 1,
+//				'departamento_id' => 1,
+//				'created_at'   => date('Y-m-d H:i:s'),
+//				'updated_at'   => date('Y-m-d H:i:s'),
+//			]);
+//		}
 	}
 }
