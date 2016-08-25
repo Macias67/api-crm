0473bbb21875b63fa881cdc433836ae42a597c31
Luis Macias <luismacias.angulo@gmail.com>
2016-08-25 15:51:37 -0500

    CAmbios menores

--- database/seeds/ContactosSeeder.php
+++ database/seeds/ContactosSeeder.php
@@ -12,6 +12,8 @@ class ContactosSeeder extends Seeder
 	 */
 	public function run()
 	{
+		$faker = Faker\Factory::create();
+		
 		DB::table('cl_contactos')->insert([
 			'id_cliente' => 1,
 			'nombre'     => 'Luis',
@@ -23,5 +25,40 @@ class ContactosSeeder extends Seeder
 			'created_at' => date('Y-m-d H:i:s'),
 			'updated_at' => date('Y-m-d H:i:s')
 		]);
+		
+		for ($i = 0; $i < 15; $i++)
+		{
+			DB::table('cl_contactos')->insert([
+				'id_cliente' => rand(1, 10),
+				'nombre'     => 'Luis',
+				'apellido'   => 'Macias',
+				'email'      => 'luismacias.angulo@gmail.com',
+				'password'   => bcrypt('qwerty'),
+				'telefono'   => '3929418119',
+				'online'     => 1,
+				'created_at' => date('Y-m-d H:i:s'),
+				'updated_at' => date('Y-m-d H:i:s'),
+				
+				'razonsocial'  => mb_strtoupper($faker->company) . ' - ' . $faker->randomNumber(4),
+				'rfc'          => mb_strtoupper(substr($faker->md5, 0, 13)),
+				'prospecto'    => $faker->randomElement([0, 1]),
+				'distribuidor' => $faker->randomElement([0, 1]),
+				'email'        => $faker->safeEmail,
+				'telefono'     => $faker->randomNumber(9) . $faker->randomNumber(1),
+				'telefono2'    => '',
+				'calle'        => $faker->streetName,
+				'noexterior'   => $faker->buildingNumber,
+				'nointerior'   => '',
+				'colonia'      => $faker->cityPrefix,
+				'cp'           => $faker->postcode,
+				'ciudad'       => $faker->city,
+				'municipio'    => '',
+				'estado'       => $faker->state,
+				'pais'         => 'México',
+				'online'       => 1,
+				'created_at'   => date('Y-m-d H:i:s'),
+				'updated_at'   => date('Y-m-d H:i:s')
+			]);
+		}
 	}
 }
--- database/seeds/CotizacionSeeder.php
+++ database/seeds/CotizacionSeeder.php
@@ -13,19 +13,19 @@ class CotizacionSeeder extends Seeder
 	public function run()
 	{
 		DB::table('ct_cotizacion')->insert([
-			'id'      => 1,
-			'cliente_id' => 1,
-			'ejecutivo_id'   => 1,
-			'contacto_id'   => 1,
-			'oficina_id' => 1,
-			'estatus_id' => 2,
-			'vencimiento' => date('Y-m-d H:i:s', time() + (60*60*24*16)),
-			'cxc' => 0,
-			'subtotal' => 5500,
-			'iva' => 880,
-			'total' => 6380,
-			'created_at' => date('Y-m-d H:i:s', time()),
-			'updated_at' => date('Y-m-d H:i:s', time())
+			'id'           => 1,
+			'cliente_id'   => rand(1, 10),
+			'ejecutivo_id' => 1,
+			'contacto_id'  => 1,
+			'oficina_id'   => 1,
+			'estatus_id'   => 2,
+			'vencimiento'  => date('Y-m-d H:i:s', time() + (60 * 60 * 24 * 16)),
+			'cxc'          => 0,
+			'subtotal'     => 5500,
+			'iva'          => 880,
+			'total'        => 6380,
+			'created_at'   => date('Y-m-d H:i:s', time()),
+			'updated_at'   => date('Y-m-d H:i:s', time())
 		]);
 	}
 }
